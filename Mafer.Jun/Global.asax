c38bf07c5fb1fa1f5c596b2fd08251045407d0b3
NOCSMSProject <371002491@qq.com>
2014-12-14 23:15:07 +0800

    Mafer.Jun's Personal Project is build

---
+++ Mafer.Jun/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mafer.Jun.MvcApplication" Language="C#" %>
---
+++ Mafer.Jun/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mafer.Jun.Startup))]
+namespace Mafer.Jun
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
